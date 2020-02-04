1d5eda71a71f8c1b5c3436c95bffeb6583844a57
Hiroki Ito <hiroki110@gmail.com>
2020-02-04 13:45:41 +0000

    add function

--- multiply/func.py
+++ multiply/func.py
@@ -1,19 +1,22 @@
 import io
 import json
+from decimal import *
 
 from fdk import response
 
 
 def handler(ctx, data: io.BytesIO=None):
-    name = "World"
+    value = Decimal('NaN')
     try:
         body = json.loads(data.getvalue())
-        name = body.get("name")
+        left = Decimal(body.get('left'))
+        right = Decimal(body.get('right'))
+        value = left * right
     except (Exception, ValueError) as ex:
         print(str(ex))
 
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
+            {"result": str(value)}),
         headers={"Content-Type": "application/json"}
     )
